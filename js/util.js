104bd4a8a3581ec6316f4737cd7d1f2afdca0689
sairessy <sairessy@gmail.com>
2024-07-14 21:52:21 -0700

    Fix wakeup api

--- js/util.js
+++ js/util.js
@@ -1,28 +1,19 @@
-async function wakeupAPI() {
-  console.log('..')
-  let num_fails = 0;
+let api_on = false;
 
-  while (!api_on && num_fails < 10) {
+async function wakeupAPI() {
+  let awake = false;
+
+  while (!awake) {
     try {
       console.log("Waking up the API...");
       const res = await fetch(api_url);
       if (res.status === 200) {
-        api_on = true;
-
-        if (api_on && localStorage.getItem('user')) {
-          document.getElementById("loader").style.display = "none";
-          console.log("Done waking up the API.");
-        }
+        awake = true;
+        console.log('Awaken: ' + awake)
+        document.getElementById("loader").style.display = "none";
       }
     } catch (error) {
       console.log(error);
-      num_fails++;
     }
   }
-
-  if (num_fails >= 10) {
-    alert("Erro no servidor.");
-  }
 }
-
-let api_on = false;
