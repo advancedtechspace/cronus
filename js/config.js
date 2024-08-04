42e9846661473a06c965b6eeb973ea961a7804b5
sairessy <sairessy@gmail.com>
2024-08-04 10:55:01 -0700

    Api_url basic fix

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const api_url =
   window.location.protocol === "https:"
-    ? "https://api-production-dbdb.up.railway.app/"
+    ? "https://api-production-dbdb.up.railway.app"
     : "http://localhost:8000";
 
 const base_url =
