32c0d202bde48735ead1615cb3f06e8b4855457d
sairessy <sairessy@gmail.com>
2024-08-03 23:44:55 -0700

    Basic fix

--- js/config.js
+++ js/config.js
@@ -13,6 +13,25 @@ const colors = {
   secondary: "#6495ed",
 };
 
+const grade_levels = [
+  { id: "0", label: "Basico" },
+  { id: "1", label: "Medio" },
+  { id: "2", label: "Bacharel" },
+  { id: "3", label: "Licenciado" },
+  { id: "4", label: "Mestre" },
+  { id: "5", label: "PhD" },
+];
+
+const areas = [
+  { id: "0", label: "Engenharia" },
+  { id: "1", label: "Administração & Gestão" },
+  { id: "2", label: "Economia e finaças" },
+  { id: "3", label: "Contabilidade e Auditoria" },
+  { id: "4", label: "Direito" },
+  { id: "5", label: "Saúde" },
+  { id: "-1", label: "Outro" },
+];
+
 const Toast = Swal.mixin({
   toast: true,
   position: "top-end",
