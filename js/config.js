091803d5a47d8af858813655d4e0f643cbec0758
sairessy <sairessy@gmail.com>
2025-10-14 15:09:14 +0200

    Change api url

--- js/config.js
+++ js/config.js
@@ -15,7 +15,7 @@ const modules = [
 
 const api_url =
   window.location.protocol === "https:"
-    ? "https://api.advancedtechspace.com"
+    ? "https://api-master.onrender.com"
     : "http://localhost:8000";
 
 //  https://api-production-dbdb.up.railway.app
